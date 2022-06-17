2d835c7616e033a1672118b5b66dd9da6a412054
m4uriciosn <mauriciososmaiarnascimento@gmail.com>
2022-06-17 10:32:56 -0300

    viewbinding do kotlin no android studio

--- app/src/main/java/com/example/simulador/MainActivity.kt
+++ app/src/main/java/com/example/simulador/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.simulador
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+//import android.widget.TextView
+import com.example.simulador.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //setContentView(R.layout.activity_main)
+        //val Hello = findViewById<TextView>(R.id.tvHello)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.tvHello.text = "teste"
     }
 }
